e9ec77451844f869dd108b753ec48283f62d3919
Ws-Peroth <a01033056809@gmail.com>
2021-03-27 23:21:10 +0900

    IDK IDK IDK...
    IDK IDK IDK...

--- FirebaseTester/Assets/DB IO cs 2/DBGetter.cs
+++ FirebaseTester/Assets/DB IO cs 2/DBGetter.cs
@@ -14,11 +14,11 @@ public class DBGetter : MonoBehaviour
     public DBUser user;
     public DatabaseReference Reference;
 
-    private void Start()
+    public void OnSignal()
     {
         Reference = FirebaseDatabase.DefaultInstance.RootReference.Reference;
 
-        Task<bool> task = new Task<bool>( () => IsUserDbExist());
+        Task<bool> task = new Task<bool>( () => Flag());
 
         task.Start();
         Debug.Log("Func Start");
@@ -28,7 +28,7 @@ public class DBGetter : MonoBehaviour
 
     }
 
-    public bool IsUserDbExist()
+    /*public bool IsUserDbExist()
     {
         print("Call Function : " + nameof(IsUserDbExist));
 
@@ -62,14 +62,17 @@ public class DBGetter : MonoBehaviour
         if (flag == 1) return true;
         return false;
 
-    }
+    }*/
 
 
-    public bool Main()
+    public bool Flag()
     {
         int flag = -2;
 
         Task<DataSnapshot> callData = GetDataAsync();
+
+        Debug.Log("Wait . . .");
+        callData.Wait();
 
         if (callData.IsCompleted)
         {
@@ -95,9 +98,8 @@ public class DBGetter : MonoBehaviour
 
     private async Task<DataSnapshot> GetDataAsync()
     {
-
         DataSnapshot data = await Reference.Child("Users").GetValueAsync();
-
+        
         return data;
     }
 
